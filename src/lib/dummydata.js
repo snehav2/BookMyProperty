cc4b11921c2f1df5d284f25d3984c8411fcb6c52
Sneha Verma <snehaverma0201@gmail.com>
2024-06-24 14:58:15 +0530

    data for app

--- src/lib/dummydata.js
+++ src/lib/dummydata.js
@@ -1,37 +1,39 @@
 export const listData = [
-  {
-    id: 1,
-    title: "A Great Apartment Next to the Beach!",
-    img: "https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    bedroom: 2,
-    bathroom: 1,
-    price: 1000,
-    address: "456 Park Avenue, London",
-    latitude: 51.5074,
-    longitude: -0.1278,
-  },
-  {
-    id: 2,
-    title: "An Awesome Apartment Near the Park! Almost too good to be true!",
-    img: "https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    bedroom: 3,
-    bathroom: 2,
-    price: 1500,
-    address: "789 Oxford Street, London",
-    latitude: 52.4862,
-    longitude: -1.8904,
-  },
-  {
-    id: 3,
-    title: "A New Apartment in the City!",
-    img: "https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    bedroom: 1,
-    bathroom: 1,
-    price: 800,
-    address: "101 Baker Street, London",
-    latitude: 53.4808,
-    longitude: -2.2426,
-  },
+  
+    {
+      id: 1,
+      title: "Mini Amazon",
+      img: "https://th.bing.com/th/id/OIP.f4zRobrNspSRr3o9Zj4hwgAAAA?w=245&h=180&c=7&r=0&o=5&dpr=2&pid=1.7",
+      bedroom: 3,
+      bathroom: 2,
+      price: 1000,
+      address: "SNN Raj GreenBay",
+      latitude: 12.85127,
+      longitude: 77.680227,
+    },
+    {
+      id: 2,
+      title: "Park h par Dost nhi khelte",
+      img: '/public/aakriti.jpeg',
+      bedroom: 2,
+      bathroom: 2,
+      price: 1500,
+      address: "Aakriti Greens,Bhopal",
+      latitude: 23.169090,
+      longitude: 77.438270,
+    },
+    {
+      id: 3,
+      title: "Har Balcony se alag view",
+      img: "/rewa.jpeg",
+      bedroom: 2,
+      bathroom: 2,
+      price: 1500,
+      address: "Singrauli towers,Rewa",
+      latitude: 24.543961,
+      longitude: 81.301552,
+    },
+  
   {
     id: 4,
     title: "Great Location! Great Price! Great Apartment!",
@@ -89,32 +91,3 @@ export const listData = [
   },
 ];
 
-export const singlePostData = {
-  id: 1,
-  title: "Beautiful Apartment",
-  price: 1200,
-  images: [
-    "https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    "https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    "https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    "https://images.pexels.com/photos/2467285/pexels-photo-2467285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  ],
-  bedRooms: 2,
-  bathroom: 1,
-  size: 861,
-  latitude: 51.5074,
-  longitude: -0.1278,
-  city: "London",
-  address: "1234 Broadway St",
-  school: "250m away",
-  bus: "100m away",
-  restaurant: "50m away",
-  description:
-    "Future alike hill pull picture swim magic chain seed engineer nest outer raise bound easy poetry gain loud weigh me recognize farmer bare danger. actually put square leg vessels earth engine matter key cup indeed body film century shut place environment were stage vertical roof bottom lady function breeze darkness beside tin view local breathe carbon swam declared magnet escape has from pile apart route coffee storm someone hold space use ahead sheep jungle closely natural attached part top grain your grade trade corn salmon trouble new bend most teacher range anybody every seat fifteen eventually",
-};
-
-export const userData = {
-  id: 1,
-  name: "John Doe",
-  img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-};
