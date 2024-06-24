eaac4e1fcbb9f7bcc1e2cfebd5cb1fbff7d66863
Sneha Verma <snehaverma0201@gmail.com>
2024-06-24 14:59:33 +0530

    Updated main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
-import NavBar from "./components/navbar/NavBar"
-import "./layout.scss"
-import HomePage from'./routes/homePage/homePage'
-function App(){
+import HomePage from "./routes/homePage/homePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./routes/listPages/listPages";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+import ProfilePage from "./routes/profilePage/profilePage";
+import Login from "./routes/login/login";
+import Register from "./routes/register/register";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        },
+        {
+          path:"/profile",
+          element:<ProfilePage/>
+        },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/register",
+          element:<Register/>
+        }
+      ]
+    }
+  ]);
+
   return (
-    <div className="layout">
-      <div className='navbar'>
-        <NavBar/>
-      </div>
-      <div className="content">
-        <HomePage/>
-      </div>
-    </div>
-  )
+
+    <RouterProvider router={router}/>
+  );
 }
 
-export default App
+export default App;
