7446e43ac3a67733b2e53387fbd6a8814a7649af
Sneha Verma <snehaverma0201@gmail.com>
2024-06-24 14:54:37 +0530

    profile update and page

---
+++ src/routes/profilePage/profilePage.jsx
@@ -0,0 +1,50 @@
+import Chat from "../../components/chat/Chat";
+import List from "../../components/list/List";
+import "./profilePage.scss";
+
+function ProfilePage() {
+  return (
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              Avatar:
+              <img
+                src="/sneha.jpg"
+                alt=""
+              />
+            </span>
+            <span>
+              Username: <b>Sneha Verma</b>
+            </span>
+            <span>
+              E-mail: <b>snehaverma0201@gmail.com
+              </b>
+            </span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
+        </div>
+      </div>
+      <div className="chatContainer">
+        <div className="wrapper">
+          <Chat/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfilePage;
