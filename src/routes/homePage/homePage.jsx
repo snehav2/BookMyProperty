cced03f14f9707d0f9e2e94e1f5051444a94aa71
Sneha Verma <snehaverma0201@gmail.com>
2024-06-24 14:56:22 +0530

    home page

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,42 +1,37 @@
 import SearchBar from "../../components/searchBar/SearchBar";
-import './homePage.scss'
+import "./homePage.scss";
 
-function HomePage(){
+function HomePage() {
   return (
-    <div className='homePage'>
-        <div className='textContainer'>
-            <div className='wrapper'>
-            <h1 className='title'>
-                Your Next Home, Just a Click Away
-            </h1>
-            <p>
-            Discover the future of real estate with BookMyProperty, your ultimate property booking appConnect instantly with sellers, agents, or potential buyers through our integrated chat system, enabling real-time communication and swift negotiations. 
-            </p>
-          <SearchBar/>
-            <div className='boxes'>
-                <div className='box'>
-                    <h1>16+</h1>
-                    <h2>Year of Experience</h2>
-                </div>
-                <div className='box'>
-                    <h1>200</h1>
-                    <h2>Agents Joined</h2>
-                </div>
-                <div className='box'>
-                    <h1>2000+</h1>
-                    <h2>Property Ready</h2>
-                </div>
-
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Your Perfect Stay with Ease</h1>
+          <p>
+          Introducing BookMyProperty, the ultimate app for all your property needs. Whether you're looking to buy or rent, HomeFinder has you covered with a seamless, user-friendly experience.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
             </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
             </div>
-            
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
         </div>
-        <div className='imgContainer'>
-            <img src='/bg.png' alt=''/>
-        </div>
-
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
