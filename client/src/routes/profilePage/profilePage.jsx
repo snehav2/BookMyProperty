1b6fe1eb3ef535b4c9738ae77d8d818b0954ad88
Sneha Verma <snehaverma0201@gmail.com>
2024-06-24 23:55:59 +0530

    Logout button Implementation

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -1,8 +1,28 @@
 import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import "./profilePage.scss";
+import { useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest";
+//import { Await, Link, useLoaderData, useNavigate } from "react-router-dom";
+//import { Suspense, useContext } from "react";
+//import { AuthContext } from "../../context/AuthContext";
 
 function ProfilePage() {
+  //const data = useLoaderData();
+
+  //const { updateUser, currentUser } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const res=apiRequest.post("/auth/logout");
+      localStorage.removeItem("user")
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="profilePage">
       <div className="details">
@@ -26,6 +46,7 @@ function ProfilePage() {
               E-mail: <b>snehaverma0201@gmail.com
               </b>
             </span>
+            <button onClick={handleLogout}>Logout</button>
           </div>
           <div className="title">
             <h1>My List</h1>
