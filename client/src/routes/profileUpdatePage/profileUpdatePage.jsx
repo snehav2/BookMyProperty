ae0cb4c38763a9940027b6c5589e1e1f033e8c30
Sneha Verma <snehaverma0201@gmail.com>
2024-06-25 14:58:04 +0530

    CRUD Operations for users

--- client/src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ client/src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,12 +1,39 @@
 import "./profileUpdatePage.scss";
-import { useContext} from "react";
+import { useState,useContext} from "react";
+import apiRequest from "../../lib/apiRequest";
 import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 function ProfileUpdatePage() {
   const { currentUser, updateUser } = useContext(AuthContext);
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const { username, email, password } = Object.fromEntries(formData);
+    
+    try {
+      const res = await apiRequest.put(`/users/${currentUser.id}`, {
+        username,
+        email,
+        password,
+        // avatar:avatar[0]
+      });
+      updateUser(res.data);
+      navigate("/profile");
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.message);
+    }
+  };
+
   return (
     <div className="profileUpdatePage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
@@ -31,6 +58,7 @@ function ProfileUpdatePage() {
             <input id="password" name="password" type="password" />
           </div>
           <button>Update</button>
+          {error && <span>error</span>}
         </form>
       </div>
       <div className="sideContainer">
